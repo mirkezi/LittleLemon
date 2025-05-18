2f21753d9917412215cb304515bf12583467f3c8
mirkezi <mirko.venali@gmail.com>
2025-05-18 23:21:36 +0200

    added DRF, serializers for menu and booking models, generic views and viewset for display

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Booking, Menu
+from rest_framework import generics, viewsets
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    '''it handles get put and delete calls'''
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    lookup_field = 'id'
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
