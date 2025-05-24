3d54d720b9051350993416f0829d1c172316f6bf
Mirko Venali <32173063+mirkezi@users.noreply.github.com>
2025-05-25 00:49:02 +0200

    Update settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'LittleLemon',
+        'NAME': 'LittleLemon', #CREATE DATABASE LittleLemon; before migration
         'USER': 'root',
-        'PASSWORD': 'mySQLPassword@mirko91!',
+        'PASSWORD': '', #This field must be implemented
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
